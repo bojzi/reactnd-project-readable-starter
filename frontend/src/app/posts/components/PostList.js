6f022b54dedda24d4d7088ece81da1a25532c420
Kristian Poslek <kristian.poslek@gmail.com>
2017-11-25 18:27:16 +0100

    post sorting

--- frontend/src/app/posts/components/PostList.js
+++ frontend/src/app/posts/components/PostList.js
@@ -3,15 +3,36 @@ import { connect } from 'react-redux'
 import { withRouter } from 'react-router-dom';
 import { votePost } from '../actions/posts';
 import Post from './Post';
+import './PostList.css';
 
 class PostList extends Component {
+    state = {
+        sorting: SORTING_VOTE_SCORE
+    };
+
+    changeSorting(sort, e) {
+        e.preventDefault();
+        this.setState({ sorting: sort });
+    }
+
     getPosts = () => {
-        const posts = Object.entries(this.props.posts);
+        let posts = Object.entries(this.props.posts);
         const {category} = this.props.match.params;
 
-        if (!category || posts.length === 0) return posts;
+        if (category && posts.length > 0) {
+            posts = posts.filter(post => post[1].category === category)
+        }
 
-        return posts.filter(post => post[1].category === category);
+        return posts.sort((post1, post2) => {
+            if (this.state.sorting === SORTING_VOTE_SCORE) {
+                return post1[1].voteScore < post2[1].voteScore
+            }
+            else if (this.state.sorting === SORTING_TIMESTAMP) {
+                return post1[1].timestamp < post2[1].timestamp
+            }
+
+            return true;
+        });
     };
 
     vote = (postId, vote) => {
@@ -32,6 +53,16 @@ class PostList extends Component {
                         )
                 }
 
+                <p>
+                    Order by:&nbsp;
+                    <a className={this.state.sorting === SORTING_VOTE_SCORE ? 'selected' : ''}
+                       href="change-sorting"
+                       onClick={(e) => { this.changeSorting(SORTING_VOTE_SCORE, e) }}>score</a> |&nbsp;
+                    <a className={this.state.sorting === SORTING_TIMESTAMP ? 'selected' : ''}
+                       href="change-sorting"
+                       onClick={(e) => { this.changeSorting(SORTING_TIMESTAMP, e) }}>timestamp</a>
+                </p>
+
                 {this.getPosts().map((post) => (
                     <div style={{marginBottom: '24px'}} key={post[1].id}>
                         <Post post={post[1]}
@@ -43,6 +74,9 @@ class PostList extends Component {
         )
     }
 }
+
+const SORTING_VOTE_SCORE = 'voteScore';
+const SORTING_TIMESTAMP = 'timestamp';
 
 function mapStateToProps({posts}) {
     return {
